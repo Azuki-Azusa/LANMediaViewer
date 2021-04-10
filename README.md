19e41b841104003e252ff20db889364db8e8740e
Azuki-Azusa <cai980129@vip.qq.com>
2021-04-10 22:44:29 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Readme
+
+A micro Web server for watching videos on PC with other devices conecting to the same LAN.
+一个用于在其他连接同一局域网的设备上观看PC上的视频的微Web服务器。
+
+## Table of Contents
+
+- [Background](#background)
+- [Install](#install)
+- [Usage](#usage)
+- [Contributors](#contributors)
+
+
+## Background
+
+家里的PS4接着电视。打火锅的时候想一边看着电脑里的视频一边吃，但电脑桌放不下火锅。于是写了这个，在PS4上开浏览器看电脑里的视频。
+
+## Install
+
+This project uses [node](http://nodejs.org) and [npm](https://npmjs.com). Go check them out if you don't have them locally installed.
+
+```sh
+$ npm install --global standard-readme-spec
+```
+
+## Usage
+
+将视频放进根目录的`public\video`文件夹中。
+可多个文件夹存入。
+
+## Contributors
+
+Azu機
+[Weibo](https://weibo.com/cj980129)
+[Twitter](https://twitter.com/c980129)
+[GitHub](https://github.com/Azuki-Azusa)
---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	showIP()
+	//gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+	loadTemplates(r)
+	route(r)
+	r.Run(":8080") // listen and serve on 0.0.0.0:8080
+}
+
+// loat static file
+func loadTemplates(r *gin.Engine) {
+	r.LoadHTMLGlob("templates/*")
+	r.StaticFS("/public", http.Dir("./public"))
+}
+
+// route table
+func route(r *gin.Engine) {
+	r.GET("", func(c *gin.Context) {
+		c.HTML(200, "index.html", nil)
+	})
+
+	r.GET("/video", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"video": getObject("public/video"),
+		})
+	})
+}
+
+// show ip of this pc
+func showIP() {
+	addrs, err := net.InterfaceAddrs()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	temp := "192.168.1.1"
+	fmt.Println("Your IP addresses which start with '192.'：")
+	for _, address := range addrs {
+
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil && ipnet.IP.String()[:3] == "192" {
+				temp = ipnet.IP.String()
+				fmt.Println(ipnet.IP.String())
+			}
+
+		}
+	}
+
+	fmt.Println("Server started, please connect to the [ip:8080] via browsers on your devices(PC, Phone, PS4, etc.)")
+	fmt.Println("For example: " + temp + ":8080")
+}
+
+// get files tree object by root path
+func getObject(root string) gin.H {
+	dir_list, e := ioutil.ReadDir(root)
+	label := strings.Split(root, "/")[len(strings.Split(root, "/"))-1]
+	path := root
+	var children []gin.H
+	if e != nil {
+		fmt.Println("read dir error")
+		return gin.H{}
+	}
+	for _, v := range dir_list {
+		if v.IsDir() {
+			children = append(children, getObject(root+"/"+v.Name()))
+		} else {
+			children = append(children, gin.H{
+				"path":  path + "/" + v.Name(),
+				"label": v.Name(),
+			})
+		}
+	}
+	return gin.H{
+		"path":     path,
+		"label":    label,
+		"children": children,
+	}
+}
